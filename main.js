b78e212892efea6ab0c30b006ce5d25ce793ee21
s4ba <sidel.h.321@gmail.com>
2017-12-16 03:23:38 +0900

    test

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-particleground(document.getElementById('#container-inner'), {
+$('#container-inner').particleground, {
     dotColor: '#ff0000',
     lineColor: '#ff0000',
     particleRadius: '10',
